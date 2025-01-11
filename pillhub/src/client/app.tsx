b2fcc9267e559f7b206360a597c676c08825caec
phil sobrepena <145170188+philsobrepena@users.noreply.github.com>
2025-01-11 10:43:25 -0800

    axios and some front end categories

--- pillhub/src/client/app.tsx
+++ pillhub/src/client/app.tsx
@@ -1,13 +1,22 @@
 import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
+// import preactLogo from './assets/preact.svg'
+// import viteLogo from '/vite.svg'
+import pillHubLogo from '/pill-hub-logo.svg'
+import axios from 'axios'
 import './app.css'
 
 const apiCall = () => {
-  fetch('http://localhost:3000/hello')
+  axios.get('http://localhost:3000/hello')
     .then((data) => console.log(data))
     .catch((error) => console.error('Error:', error));
 }
+
+const categories = [
+  'Men', 'Women', 'Children',
+  'Mood', 'Sleep', 'Energy',
+  'Brain', 'Immune', 'Digestive',
+  'Skin', 'Hair', 'Weight', 'Pain'
+]
 
 export function App() {
   const [count, setCount] = useState(0)
@@ -15,27 +24,38 @@ export function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <img src={pillHubLogo} alt="pillHub logo" />
+        {/* <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} class="logo" alt="Vite logo" />
         </a>
         <a href="https://preactjs.com" target="_blank">
           <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
+        </a> */}
       </div>
-      <h1>Vite + Preact</h1>
+      <h1>pillhub</h1>
+      <h2>the #1 way to find your supplements.</h2>
       <div class="card">
+        <div>
+        <label for="search">Search or select your categories</label>
+        <input type="text" style={{ width: '100%' }} placeholder="Search for a supplement" />
+        </div>
+        <div class="categories">
+          {categories.map((category) => (
+            <button>{category}</button>
+          ))}
+        </div>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <button onClick={apiCall}>
           api call
         </button>
-        <p>
+        {/* <p>
           Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
+        </p> */}
       </div>
       <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
+        browse supplements by name, brand, or category
       </p>
     </>
   )
